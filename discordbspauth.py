30d72b46c16747c503a3754f02f6f0f2116dc02e
mattsh3n <112284733+mattsh3n@users.noreply.github.com>
2022-09-01 11:51:07 +0200

    Update discordbspauth.py

--- discordbspauth.py
+++ discordbspauth.py
@@ -8,7 +8,8 @@ async def on_ready():
     print('The bot is ready!')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing,name="BlockStarPlanet"))
 @client.command()
-async def Activation(ctx,hwid):
-    if hwid != None:
+async def Activation(ctx):
+    args = ctx.message.content.split(" ")
+    if(len(args) == 2):
         requests.get(f"http://suerte.ct8.pl/AuthService.php?hwid={hwid}&dcid={ctx.author.id}&method=addnew")
 client.run("MTAxNDgyNjg1ODQ2ODA4OTg3Ng.GrgYCX.b4xmMPQOHB9KuwOg0MWJZcn8XXJOqvr1uzEfEk")
