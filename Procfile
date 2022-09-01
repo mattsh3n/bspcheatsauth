ff68314ac94a253795daa51a30e675a0c9791ff6
mattsh3n <112284733+mattsh3n@users.noreply.github.com>
2022-09-01 11:46:26 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python discordbspauth.py
---
+++ discordbspauth.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import requests
+client = commands.Bot(command_prefix="?")
+client.remove_command("help")
+@client.event
+async def on_ready():
+    print('The bot is ready!')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing,name="BlockStarPlanet"))
+@client.command()
+async def Activation(ctx,hwid):
+    if hwid != None:
+        requests.get(f"http://suerte.ct8.pl/AuthService.php?hwid={hwid}&dcid={ctx.author.id}&method=addnew")
+client.run("MTAxNDgyNjg1ODQ2ODA4OTg3Ng.GrgYCX.b4xmMPQOHB9KuwOg0MWJZcn8XXJOqvr1uzEfEk")
